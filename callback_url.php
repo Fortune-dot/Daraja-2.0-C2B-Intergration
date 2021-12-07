bfbd714a2e5eeb14b58b95e98a95e0e5f35e9944
Fortune <fortunelangat54@gmail.com>
2021-12-07 11:56:41 +0300

    new

--- callback_url.php
+++ callback_url.php
@@ -1,6 +1,21 @@
 <?php
-  $stkCallbackResponse = file_get_contents('php://input');
-  $logFile = "stkPushCallbackResponse.json";
-  $log = fopen($logFile, "a");
-  fwrite($log, $stkCallbackResponse);
-  fclose($log);
+ 		header("Content-Type: application/json");
+
+     $response = '{
+         "ResultCode": 0, 
+         "ResultDesc": "Confirmation Received Successfully"
+     }';
+ 
+     // DATA
+     $mpesaResponse = file_get_contents('php://input');
+ 
+     // log the response
+     $logFile = "M_PESAConfirmationResponse.txt";
+ 
+     // write to file
+     $log = fopen($logFile, "a");
+ 
+     fwrite($log, $mpesaResponse);
+     fclose($log);
+ 
+     echo $response;
