af28539f370b2c0d3f07ced5812f1a6563d01c29
Fortune <fortunelangat54@gmail.com>
2021-12-07 11:10:14 +0300

    second commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'http://codestar.co.ke/projects/MPESA_API/callback_url.php';  
+  $CallBackURL = 'https://morning-forest-72309.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
